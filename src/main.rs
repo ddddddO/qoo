b31a3013f5077cf6582aa120c4015339dc255fb2
ddddddO <lbfdeatq@gmail.com>
2022-01-01 00:44:17 +0900

    base

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let query = Query::select("col1".to_string()).from("table1".to_string());
-    println!("{}", query.to_string());
+    let select_query = Query::select("col1".to_string()).from("table1".to_string());
+    println!("{}", select_query.to_string());
+
+    let delete_query = Query::delete().from("test1".to_string());
+    println!("{}", delete_query.to_string());
+}
+
+trait Base {
+    fn to_string(self) -> String;
 }
 
 struct Query {
@@ -14,12 +21,20 @@ impl Query {
         }
     }
 
+    fn delete() -> Query {
+        Query {
+            q: format!("delete"),
+        }
+    }
+
     fn from(self, table: String) -> Query {
         Query {
             q: format!("{} from {}", self.q , table),
         }
     }
+}
 
+impl Base for Query {
     fn to_string(self) -> String {
         self.q
     }
