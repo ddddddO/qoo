cf1d58d86cd0038253bc6406fd6694df676694b7
ddddddO <lbfdeatq@gmail.com>
2022-01-01 14:27:58 +0900

    assert

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use qoo::delete::*;
 fn main() {
     // TODO: .to_vec()無くしたい
     let select_query = SelectBuilder::select(["col0", "col1", "col2"].to_vec()).from("table1");
-    println!("{}", select_query.to_sql());
+    assert_eq!(select_query.to_sql(), "select col0, col1, col2 from table1".to_string());
 
     let delete_query = DeleteBuilder::delete().from("test1");
-    println!("{}", delete_query.to_sql());
+    assert_eq!(delete_query.to_sql(), "delete from test1".to_string());
 }
